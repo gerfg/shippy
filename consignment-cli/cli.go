8b7a8a0b912f9395ffc63e911c9e1d85c21d4cae
Geraldo <geraldo.figueidosj@gmail.com>
2019-10-03 21:23:47 -0300

    getConsignments added in protobuf file

---
+++ consignment-cli/cli.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/gerfg/shippy/consignment-service/proto/consignment"
+	"google.golang.org/grpc"
+)
+
+const (
+	address         = "localhost:50051"
+	defaultFilename = "consignment.json"
+)
+
+func parseFile(file string) (*consignment.Consignment, error) {
+	var cons *consignment.Consignment
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	json.Unmarshal(data, &cons)
+	return cons, err
+}
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Did not connect: %v", err)
+	}
+	defer conn.Close()
+	client := consignment.NewShippingServiceClient(conn)
+
+	// contact the server and print out its response
+	file := defaultFilename
+	if len(os.Args) > 1 {
+		file = os.Args[1]
+	}
+
+	cons, err := parseFile(file)
+	if err != nil {
+		log.Fatalf("Could not parse file: %v", err)
+	}
+
+	r, err := client.CreateConsignment(context.Background(), cons)
+	if err != nil {
+		log.Fatalf("Could not greet: %v", err)
+	}
+	log.Printf("Created %t", r.Created)
+}
